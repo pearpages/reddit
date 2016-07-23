d5a80e1901c1c4d61b72a40cb70d47126314c946
pearpages <pere@soms.cat>
2016-07-23 18:38:05 +0200

    minor

--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
-// your code goes here
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'reddit',
+    template: `
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a Link</h3>
+
+        <div class="field">
+            <label for="title">Title: </label>
+            <input name="title" [(ngModel)]="newtitle">
+        </div>
+        <div class="field">
+            <label for="link">Link: </label>
+            <input name="link" [(ngModel)]="newlink">
+        </div>
+        
+        <button (click)="addArticle()" class="ui positive right floated button">Submit link</button>
+    </form>
+    `
+})
+class RedditApp {
+
+    newtitle: string;
+    newlink: string;
+
+    constructor() {}
+
+    addArticle(): void {
+        console.log(this.newtitle,this.newlink);
+    }
+}
+
+bootstrap(RedditApp);
